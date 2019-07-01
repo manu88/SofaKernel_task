897bdbdea31b23b0c4db24513b3ee8288259e978
admingie <admingie@imacdeadmingie2.sesam.loc>
2019-07-01 12:01:09 +0200

    added unit tests

---
+++ UnitTests/tests/main.cpp
@@ -0,0 +1,12 @@
+//
+//  main.cpp
+//  UnitTests
+//
+//  Created by admingie on 01/07/2019.
+//  Copyright © 2019 admingie. All rights reserved.
+//
+
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+
+
